83d184849b8c9d98f315671c52eaa6ba5cee83d4
Michael-Guerre <mickguerre@aol.com>
2016-04-17 20:26:20 +0200

    Added files via upload

--- Grille.java
+++ Grille.java
@@ -14,49 +14,15 @@ import static java.awt.MouseInfo.getPointerInfo;
 public class Grille {
     final static Scanner input= new Scanner(System.in);
 
-    int dimensionCarre=50;
-    private int coord1D;
+
     private Case[] grille;
     Grille(){
         grille=new Case[100];
-    }
-
-    public JPanel affiche(){
-        JPanel grille = new JPanel();
-        JPanel afficheButton = new JPanel(new GridLayout(10,10));
-        JPanel[] gridPanel = new JPanel[100];
-        for (int i=0;i<gridPanel.length;i++){
-            gridPanel[i]=new JPanel();
-            gridPanel[i].setPreferredSize(new Dimension(dimensionCarre, dimensionCarre));
-            gridPanel[i].setBorder(BorderFactory.createLineBorder(Color.black));
-            afficheButton.add(gridPanel[i]);
+        for (int i=0;i<100;i++){
+            int x=i%10;
+            int y=i/10;
+            grille[i]=new Case(x,y);
         }
-        afficheButton.addMouseListener(new MouseAdapter() {
-            private Color background;
-            private double coordX;
-            private double coordY;
-            @Override
-            public void mousePressed(MouseEvent e) {
-                //calcul la case de la grille
-                coordX=(e.getX()-grille.getX())/dimensionCarre;
-                coordY=(e.getY()-grille.getY())/dimensionCarre;
-                coord1D=(int)(coordX+coordY*10);
-                //permet de tester en changeant la couleur
-                System.out.println(coordX+" "+coordY+" "+coord1D);
-                background = gridPanel[coord1D].getBackground();
-                gridPanel[coord1D].setBackground(Color.RED);
-                gridPanel[coord1D].repaint();
-
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                //remet la case de la couleur originelle
-                gridPanel[coord1D].setBackground(background);
-            }
-        });
-        grille.add(afficheButton);
-        return grille;
     }
 }
 
--- Main_Grille_test.java
+++ Main_Grille_test.java
@@ -3,12 +3,11 @@ import javax.swing.*;
 public class Main_Grille_test {
     public static void main(String[] args) {
         //juste pour tester
-        Grille J1 = new Grille();
-        JFrame test = new JFrame();
-        test.setContentPane(J1.affiche());
-        test.pack();
-        test.setVisible(true);
+        ConteneurGrille J1 = new ConteneurGrille();
 
-    }
+        JFrame test = new JFrame();
+        test.setContentPane(J1);
+        test.pack();
+        test.setVisible(true);}
 
 }
