6037e0a9760591d248026a397113f8d55e69c70a
Michael-Guerre <mickguerre@aol.com>
2016-04-24 11:59:51 +0200

    Added files via upload

--- Attente.java
+++ Attente.java
@@ -5,10 +5,9 @@
     import java.awt.*;
 
     public class Attente extends JFrame {
-        public Attente() {
+        public Attente(String Joueur) {
 
-            //creerWidgetJoueur1();
-            creerWidgetJoueur2();
+            creerWidgetJoueur(Joueur);
 
             //pack();                                          // Fixe la taille par défaut
             setSize(800,800);
@@ -16,9 +15,9 @@
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Gestion de la fermeture
         }
 
-        public void creerWidgetJoueur1() {
+        public void creerWidgetJoueur(String nomJoueur) {
             //Message
-            JLabel lab = new JLabel("Joueur 1 à toi de jouer",SwingConstants.CENTER); // SwingConstants.center sert à centrer le texte
+            JLabel lab = new JLabel(nomJoueur+" à toi de jouer",SwingConstants.CENTER); // SwingConstants.center sert à centrer le texte
             //Une méthode get pour récupérer le nom du joueur ?
             //JLabel lab = new JLabel(+conteneur.getJoueur1().getText()+ "à toi de jouer",SwingConstants.CENTER);
             // Affichage du bouton prêt
@@ -28,25 +27,13 @@
             pano.add(pret, BorderLayout.SOUTH);
             setContentPane(pano);
         }
-        public void creerWidgetJoueur2() {
-            //Message
-            JLabel lab2 = new JLabel("Joueur 2 à toi de jouer",SwingConstants.CENTER); // SwingConstants.center sert à centrer le texte
-            //Une méthode get pour récupérer le nom du joueur ?
-            //JLabel lab2 = new JLabel(+conteneur.getJoueur2().getText()+ "à toi de jouer",SwingConstants.CENTER);
-            // Affichage du bouton prêt
-            JButton pret2 = new JButton("Prêt ?");;
-            JPanel pano2 = new JPanel(new BorderLayout());
-            pano2.add(lab2, BorderLayout.CENTER);
-            pano2.add(pret2, BorderLayout.SOUTH);
-            setContentPane(pano2);
 
-        }
         public static void main(String[] args) {
 
             javax.swing.SwingUtilities.invokeLater( new Runnable() {
 
                 public void run() {
-                    Attente f = new Attente();
+                    Attente f = new Attente("Nom_fictif");
                 }
 
             });
