9d99e822e22ca411661865ad5d37d93237648388
AntoinePy <guildantoine@hotmail.fr>
2016-05-09 09:48:10 +0200

    Tableau des scores + ajout dans les classes des methodes get

---
+++ src/Modele/DemoAnimation.java
@@ -0,0 +1,45 @@
+package Modele;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+/**
+ * Created by ANTOINE on 09/05/2016.
+ */
+public class DemoAnimation extends JFrame {
+    JPanel contentPane;
+    JLabel imageLabel = new JLabel();
+    JLabel headerLabel = new JLabel();
+
+    public DemoAnimation() {
+        try {
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            contentPane = (JPanel) getContentPane();
+            contentPane.setLayout(new BorderLayout());
+            setSize(new Dimension(400, 300));
+            setTitle("Raté");
+            // add the header label
+            headerLabel.setFont(new java.awt.Font("Comic Sans MS", Font.BOLD, 16));
+            headerLabel.setText("Raté");
+            contentPane.add(headerLabel, java.awt.BorderLayout.NORTH);
+            // add the image label
+            ImageIcon ii = new ImageIcon(this.getClass().getResource(
+                    "eau.gif"));
+            imageLabel.setIcon(ii);
+            contentPane.add(imageLabel, java.awt.BorderLayout.CENTER);
+            // show it
+            this.setLocationRelativeTo(null);
+            this.setVisible(true);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new DemoAnimation();
+    }
+
+}
