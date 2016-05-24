a1eea93c483e1bd8ec1838848ce9b987108d4ea7
fparty2 <partyflorian@gmail.com>
2016-05-24 11:37:56 +0200

    correction bug sur la sauvegarde

---
+++ src/Vue/ConteneurVictoire.java
@@ -0,0 +1,28 @@
+package Vue;
+
+import Modele.Jeu;
+
+import javax.swing.*;
+import java.util.ResourceBundle;
+
+/**
+ * Created by fparty2 on 24/05/16.
+ */
+public class ConteneurVictoire extends JPanel{
+    public ConteneurVictoire(Jeu jeu){
+        ResourceBundle texteInternational = ResourceBundle.getBundle("traductions.EcouteurConteneurGrillePhaseTir");
+
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JLabel messageVictoire = new JLabel(texteInternational.getString("bravo")+
+                jeu.getJoueurConcerne().getNomJoueur()+
+                texteInternational.getString("gagne"));
+        add(messageVictoire);
+
+        TableauScores tableauScore = new TableauScores(jeu);
+        add(tableauScore);
+
+
+    }
+}
